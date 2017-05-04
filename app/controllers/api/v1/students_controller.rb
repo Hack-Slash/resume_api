87b807fe821689085784289f6cefe69e75a0dda3
Josh <jspringxc@gmail.com>
2017-05-04 19:49:25 -0400

    add update and destroy actions

--- app/controllers/api/v1/students_controller.rb
+++ app/controllers/api/v1/students_controller.rb
@@ -5,12 +5,32 @@ class Api::V1::StudentsController < ApplicationController
     render 'index.json.jbuilder'
   end
 
-  def update
-    @student = Stud
-  end
-
   def show
     @student = Student.find_by(id: params[:id])
     render 'show.json.jbuilder'
+  end
+
+  def update
+    @student = Student.find_by(id: params[:id])
+    @student.update(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number],
+      short_bio: params[:short_bio],
+      linkedin_url: params[:linkedin_url],
+      twitter_handle: params[:twitter_handle],
+      personal_web_url: params[:personal_web_url],
+      online_resume_url: params[:online_resume_url],
+      github_url: params[:github_url],
+      photo: params[:photo]
+    )
+    render 'show.json.jbuilder'
+  end
+
+  def destroy
+    @student = Student.find_by(id: params[:id])
+    @student.destroy
+    redirect_to '/students'
   end
 end
